7b6db59065e5d00558e14e472866e572d589c8ba
Oskar Codes <oskarbertrand@icloud.com>
2020-04-26 16:52:41 +0200

    Create mario-hard.js

---
+++ week 1/mario-hard.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+
+var n = prompt("Height: ");
+
+while (isNaN(n) || n.trim() === "" || parseInt(n) < 1 || parseInt(n) > 8) {
+  n = prompt("Height: ");
+}
+
+n = parseInt(n);
+
+for (var i=1; i<=n; i++) {
+  console.log("#".repeat(i).padStart(n," ") + "  " + "#".repeat(i));
+}
